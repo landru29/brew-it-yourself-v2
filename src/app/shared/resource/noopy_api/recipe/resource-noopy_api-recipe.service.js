2cef546a94e816c9ea9bc402624121b4ca36d121
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2016-02-16 13:41:34 +0100

    crud

--- src/app/shared/resource/noopy_api/recipe/resource-noopy_api-recipe.service.js
+++ src/app/shared/resource/noopy_api/recipe/resource-noopy_api-recipe.service.js
@@ -1,26 +1,57 @@
-angular.module('brewItYourself').service('ResourceNoopyApiRecipe', function($http, $q, Noopy) {
+angular.module('brewItYourself').service('ResourceNoopyApiRecipe', function($http, $q) {
 
   this.get = function(id) {
     return $q(function(resolve, reject) {
-      reject('Not implemented');
+      $http.get('noopy-api/me/beer/read/' + id).then(function(response) {
+        var newRecipe = response.data.data;
+        recipe.id = _id;
+        return resolve(recipe);
+      }, function(err) {
+        return reject(err);
+      });
     });
   };
 
   this.create = function(model) {
+    model = model ? model : {};
     return $q(function(resolve, reject) {
-      reject('Not implemented');
+      $http.post('noopy-api/me/beer', {
+        name: model.name ? model.name : 'New recipe',
+        date: model.date ? model.date : (new Date()).toISOString(),
+        steps: model.steps ? model.steps : []
+      }).then(function(response) {
+        var newRecipe = response.data.data;
+        newRecipe.id = _id;
+        return resolve(newRecipe);
+      }, function(err) {
+        return reject(err);
+      });
     });
   };
 
   this.remove = function(id) {
     return $q(function(resolve, reject) {
-      reject('Not implemented');
+      $http.delete('noopy-api/me/beer/' + id).then(function(response) {
+        return resolve(response.data);
+      }, function(err) {
+        return reject(err);
+      });
     });
   };
 
   this.save = function(recipe) {
     return $q(function(resolve, reject) {
-      reject('Not implemented');
+      $http.put('noopy-api/me/beer/' + recipe.id, {
+        name: recipe.name ? model.name : 'New recipe',
+        date: recipe.date ? model.date : (new Date()).toISOString(),
+        steps: recipe.steps ? model.steps : []
+      }).then(function(response) {
+        var newRecipe = response.data.data;
+        newRecipe.id = _id;
+        return resolve(newRecipe);
+      }, function(err) {
+        return reject(err);
+      });
     });
   };
 
@@ -29,10 +60,12 @@ angular.module('brewItYourself').service('ResourceNoopyApiRecipe', function($htt
     if ((options) && (options.perPage)) params.perPage = options.perPage;
     if ((options) && (options.page)) params.page = options.page;
     return $q(function(resolve, reject) {
-      var url = 'noopy-api/me/beer/list';
-      $http.get(url).then(function(response) {
-        console.log(response);
-        return resolve(response.data.data);
+      $http.get('noopy-api/me/beer/list').then(function(response) {
+        var recipeList = _.isArray(response.data.data) ? response.data.data : [];
+        recipeList.forEach(function(recipe){
+          recipe.id = recipe._id;
+        });
+        return resolve(recipeList);
       }, function(err) {
         return reject(err);
       });
