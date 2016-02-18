10e97fe5623e722110ada99b827a6d9583755227
Cyrille M <cmeichel@free.fr>
2016-02-18 21:31:25 +0100

    fix save recipe

--- src/app/shared/resource/noopy_api/recipe/resource-noopy_api-recipe.service.js
+++ src/app/shared/resource/noopy_api/recipe/resource-noopy_api-recipe.service.js
@@ -15,14 +15,15 @@ angular.module('brewItYourself').service('ResourceNoopyApiRecipe', function($res
   this.create = function(model) {
     model = model ? model : {};
     return $resource('noopy-api/me/beer', {}, {
-      post: {
+      create: {
+        method: 'POST',
         interceptor: {
           response: function(response) {
             return response.data.data;
           }
         }
       }
-    }).post({
+    }).create({}, {
         name: model.name ? model.name : 'New recipe',
         date: model.date ? model.date : (new Date()).toISOString(),
         steps: model.steps ? model.steps : []
@@ -35,14 +36,15 @@ angular.module('brewItYourself').service('ResourceNoopyApiRecipe', function($res
 
   this.save = function(recipe) {
     return $resource('noopy-api/me/beer/:recipeId', {}, {
-      put: {
+      save: {
+        method: 'PUT',
         interceptor: {
           response: function(response) {
             return response.data.data;
           }
         }
       }
-    }).put({recipeId: id}, {
+    }).save({recipeId: recipe.id}, {
         name: recipe.name ? recipe.name : 'New recipe',
         date: recipe.date ? recipe.date : (new Date()).toISOString(),
         steps: recipe.steps ? recipe.steps : []
